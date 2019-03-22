f853fe4eb0dbe98ee51e7700b0cdc7cd37cddd0d
huxi <1324907549@qq.com>
2019-03-22 21:25:56 +0800

    Initial Commit

---
+++ Logger/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Logger
+//
+//  Created by huxi on 2019/3/22.
+//  Copyright © 2019 huxi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
